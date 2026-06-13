7acf4ce0a15b315ab4ad9a929eb1aa6622529ebd
khaledmanea <khaledmanea0077@gmail.com>
2026-06-13 14:05:07 +0300

    Add Posts controller with 7 CRUD actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,3 +1,29 @@
 class PostsController < ApplicationController
-  # your code goes here
+  # GET /posts
+  def index
+  end
+
+  # GET /posts/:id
+  def show
+  end
+
+  # GET /posts/new
+  def new
+  end
+
+  # GET /posts/:id/edit
+  def edit
+  end
+
+  # POST /posts
+  def create
+  end
+
+  # PATCH /posts/:id
+  def update
+  end
+
+  # DELETE /posts/:id
+  def destroy
+  end
 end

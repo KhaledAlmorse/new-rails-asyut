2b51d710f0ec487a0b776d66e73f2aec91ee1136
khaledmanea <khaledmanea0077@gmail.com>
2026-06-13 14:07:50 +0300

    Add User model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  validates :name, presence: true
+  validates :dob, presence: true
+  validates :phone_number, presence: true
+  validates :email, presence: true,
+                    uniqueness: true,
+                    format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i,
+                              message: "must be a valid email address" }
+end

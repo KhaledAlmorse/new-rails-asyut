32f8105aa7a0c8996df9ae8ae5abfd52c8e43c04
khaledmanea <khaledmanea0077@gmail.com>
2026-06-13 14:10:44 +0300

    Add Users controller with CRUD logic

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,54 @@
+class UsersController < ApplicationController
+  # GET /users
+  def index
+    @users = User.all
+  end
+
+  # GET /users/:id
+  def show
+    @user = User.find(params[:id])
+  end
+
+  # GET /users/new
+  def new
+    @user = User.new
+  end
+
+  # GET /users/:id/edit
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  # POST /users
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render :new
+    end
+  end
+
+  # PATCH /users/:id
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to @user
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /users/:id
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :dob, :email, :phone_number, :address)
+  end
+end

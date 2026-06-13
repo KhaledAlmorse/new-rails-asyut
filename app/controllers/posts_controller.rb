07018bb99bcb446c9ff1c4abe363d7a0d188a40b
khaledmanea <khaledmanea0077@gmail.com>
2026-06-13 14:06:03 +0300

    Fill Posts controller with CRUD logic

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,29 +1,54 @@
 class PostsController < ApplicationController
   # GET /posts
   def index
+    @posts = Post.all
   end
 
   # GET /posts/:id
   def show
+    @post = Post.find(params[:id])
   end
 
   # GET /posts/new
   def new
+    @post = Post.new
   end
 
   # GET /posts/:id/edit
   def edit
+    @post = Post.find(params[:id])
   end
 
   # POST /posts
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
   end
 
   # PATCH /posts/:id
   def update
+    @post = Post.find(params[:id])
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render :edit
+    end
   end
 
   # DELETE /posts/:id
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
